5c32de59a8689a9bb1ea03154998e474658e9677
meghana27N <33761770+meghana27N@users.noreply.github.com>
2020-11-25 00:44:09 +0530

    Update inst_freq.m

--- codes/fearure_extraction/inst_freq.m
+++ codes/fearure_extraction/inst_freq.m
@@ -6,35 +6,31 @@ M = length (file);
 for k = 1:M  
     [x,Fs]= audioread(fullfile(file(k).name)); 
 
-% performing the HILBERT TRANSFORM
+% HILBERT TRANSFORM
    hx = hilbert(x);
-% calculating the INSTANTANEOUS AMPLITUDE (ENVELOPE)
+% INSTANTANEOUS AMPLITUDE
    inst_amp = abs(hx);
-% calculating the INSTANTANEOUS FREQUENCY
+% INSTANTANEOUS FREQUENCY
    instt_freq = diff(unwrap(angle(hx)))/((1/Fs)*2*pi);
-   %figure;
-   %subplot(3,1,1), plot(x), title('Modulated signal')
-   %subplot(3,1,2), plot(inst_amp), title('Instantaneous amplitude')
 
-   %disp(inst_amp)
    
-   filename = sprintf('ia_kannada_%d.txt',k);
+   filename = sprintf('ia_kannada_%d.txt',k); %use the name of the language file it's being used for
    %fprintf('instant amp \n');
    fprintf('loop:%i',k);
-   %n = size(instt_freq);
-   %for ii = 1:n
-   %fid = fopen(sprintf('if_oriya_%d.txt',k),'w');
-   %fprintf(fid,'%6.4f\n',instt_freq);
-   %filename = sprintf('pitches_%d.txt',k);
    fid = fopen(filename,'wt');
    fprintf(fid,'%.8f\n', inst_amp);
    fclose(fid);
 
-   %end
-   %disp(inst_amp)
-   %filename = sprintf('%s_%d','ia_oriya_',k);
-   %writematrix(inst_amp,filename);
-   %fprintf('%i\n',instt_freq);
    
+   filename2 = sprintf('if_kannada_%d.txt',k);
+   %fprintf('instant amp \n');
+   fprintf('loop:%i',k);
+   fid2 = fopen(filename2,'wt');
+   fprintf(fid2,'%.8f\n', instt_freq);
+   fclose(fid2);
+
+   %figure;
+   %subplot(3,1,1), plot(x), title('Modulated signal')
+   %subplot(3,1,2), plot(inst_amp), title('Instantaneous amplitude')
    %subplot(3,1,3), plot(instt_freq),title('Instantaneous frequency')
 end
